887a71fb63b9fbd9fe57d47ddf31fc67ea3d1402
umerTechQa <109993016+umerTechQa@users.noreply.github.com>
2023-11-20 18:57:09 +0500

    This is new change on playwright

--- Playwright.com/src/main/java/Playwright_series/Newtestcase.java
+++ Playwright.com/src/main/java/Playwright_series/Newtestcase.java
@@ -2,9 +2,19 @@ package Playwright_series;
 
 import org.testng.annotations.Test;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
 public class Newtestcase {
     @Test
 	public void new001() {
+    	Playwright playwright=Playwright.create();
+    	Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		Page page=browser.newPage();
+		page.navigate("https://www.ebay.com/");		
 		System.out.println(" This is new test cases added !");
+		
 	}
 }
