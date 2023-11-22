e40ed02c5a515aa399ef8dae7e8252e719130138
umerTechQa <109993016+umerTechQa@users.noreply.github.com>
2023-11-22 12:14:23 +0500

    Playwright tracing

---
+++ Playwright.com/src/main/java/Playwright_series/Playwright_tracing.java
@@ -0,0 +1,28 @@
+package Playwright_series;
+
+import java.nio.file.Paths;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Tracing;
+
+public class Playwright_tracing {
+
+	public void tracing() {
+		Playwright playwright=Playwright.create();
+		Browser broswer=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		BrowserContext brcontext=broswer.newContext();
+		brcontext.tracing().start(new Tracing.StartOptions()
+				  .setScreenshots(true)
+				  .setSnapshots(true)
+				  .setSources(true));
+
+		Page page=brcontext.newPage();
+		page.navigate("https://www.facebook.com/");
+		brcontext.tracing().stop(new Tracing.StopOptions()
+				  .setPath(Paths.get("trace.zip")));
+	}
+}
